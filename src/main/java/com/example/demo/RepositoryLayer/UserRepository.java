2ffddd0584c88447eaea551362eaf6c444e408ff
Joaco4200 <joaquinscarone1012@gmail.com>
2024-11-20 02:08:54 -0300

    safe commit

--- src/main/java/com/example/demo/RepositoryLayer/UserRepository.java
+++ src/main/java/com/example/demo/RepositoryLayer/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> { //UserRepository hereda metodos CRUD de JPARepository.Espera tipo de la entidad y tipo de la claveprimaria.
     User findByauth0id(String auth0id);
-//    User findById(int id);
 }
 
 
